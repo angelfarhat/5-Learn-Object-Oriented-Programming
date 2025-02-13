f7ceabeb78bd9cb5f54b33250e0bd6e4c8150226
angelfarhat <devtahraf@gmail.com>
2025-02-13 16:09:24 -0600

    CH1:L12

---
+++ constructor_1.py
@@ -0,0 +1,8 @@
+class Wall:
+    def __init__(self, depth, height, width):
+        self.depth = depth
+        self.height = height
+        self.width = width 
+        self.volume = self.volume = self.depth * self.height * self.width
+     
+wall = Wall(2,3,4) #need this information to get data 
